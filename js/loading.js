c8f1484a8433eb12277ba62d87f96d61663d9715
Aboubkr <aboubkrmohamed86@gmail.com>
2024-03-03 19:02:17 +0200

    task

---
+++ js/loading.js
@@ -0,0 +1,15 @@
+const loading =document.getElementById('loading');
+const section =document.getElementById('section');
+
+function myFunction() {
+setTimeout(()=>{
+document.getElementById("loading").style.display = "none";
+// document.getElementById("section").style.height = "auto";
+
+
+}, 3000);
+}
+
+myFunction()
+
+
